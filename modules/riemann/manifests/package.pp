10343b8640c3ae8fd109da0d9e58f5e1a23ba3c4
Patrick O'Doherty <p@trickod.com>
2013-03-17 14:46:17 -0700

    Beginnings of Debian-friendly Riemann puppet scripts

--- modules/riemann/manifests/package.pp
+++ modules/riemann/manifests/package.pp
@@ -9,23 +9,30 @@ class riemann::package($version) {
   }
 
   wget::fetch { 'download_riemann':
-    source      => "http://aphyr.com/riemann/riemann-$version.tar.bz2",
-    destination => "/usr/local/src/riemann-$version.tar.bz2",
-    before      => Exec['untar_riemann'],
+    source      => "http://aphyr.com/riemann/riemann-$version.tar.deb",
+    destination => "/usr/local/src/riemann-$version.tar.deb",
+    before      => Exec['dpkg_install_riemann'],
   }
 
-  exec { 'untar_riemann':
-    command => "tar xvfj /usr/local/src/riemann-$version.tar.bz2",
-    cwd     => '/opt',
-    creates => "/opt/riemann-$version",
-    path    => ['/bin',],
-    before  => File['/opt/riemann'],
+  exec { 'dpkg_install_riemann':
+    command => "dpkg -i /usr/local/src/riemann-$version.deb",
+    cwd     => "/opt",
+    creates => "/etc/init.d/riemann",
+    path    => ['/bin', '/usr/bin'],
   }
 
-  file { '/opt/riemann':
-    ensure => link,
-    target => "/opt/riemann-$version",
-    notify => Service['riemann'],
-  }
+  # exec { 'untar_riemann':
+  #   command => "tar xvfj /usr/local/src/riemann-$version.tar.bz2",
+  #   cwd     => '/opt',
+  #   creates => "/opt/riemann-$version",
+  #   path    => ['/bin',],
+  #   before  => File['/opt/riemann'],
+  # }
+
+  # file { '/opt/riemann':
+  #   ensure => link,
+  #   target => "/opt/riemann-$version",
+  #   notify => Service['riemann'],
+  # }
 
 }
--- modules/riemann/manifests/service.pp
+++ modules/riemann/manifests/service.pp
@@ -1,12 +1,6 @@
 class riemann::service($config_file) {
   file { '/etc/init.d/riemann':
-    ensure => link,
-    target => '/lib/init/upstart-job',
-  }
-
-  file { '/etc/init/riemann.conf':
-    ensure  => present,
-    content => template('riemann/init/riemann.conf.erb')
+    ensure => exists,
   }
 
   service {'riemann':
@@ -17,10 +11,9 @@ class riemann::service($config_file) {
     provider   => upstart,
     require    => [
       File['/etc/init.d/riemann'],
-      File['/etc/init/riemann.conf'],
       Class['riemann::package'],
     ],
   }
-
-  File['/etc/init/riemann.conf'] ~> Service['riemann']
+  File['/etc/init.d/riemann'] ~> Service['riemann']
+  # File['/etc/init/riemann.conf'] ~> Service['riemann']
 }
